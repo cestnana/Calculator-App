18ec45cdf341fe95e8aeb4a34cb45a9e88d1c5a8
Remi Sha <105662433+cestnana@users.noreply.github.com>
2024-10-11 04:23:03 +0800

    add code snippets for temporary backup

---
+++ code_snippets.py
@@ -0,0 +1,16 @@
+# input validation (handle NaN)
+
+# def input_number(arg):
+#   user_input = arg
+#   while True:
+    
+#     try:
+#       value = float(user_input)
+#       print("The value you entered is:", value)
+#       break  # Input correctly will exit the loop
+#     except ValueError:
+#       # catch the error and handle it
+#       print("What you entered is not a number. Please re-enter it.")
+        
+#   return float(user_input)
+
